7db812600274bd839e4eca018b69366de646fa05
narcisseisaker <narcisseisaker@gmail.com>
2026-01-17 20:25:06 +0100

    github repository integration

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,45 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in"> Hi, I&apos;m</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              {" "}
+             MOSSY MOSSY
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              
+              Isaker Narcisse
+            </span>
+          </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+            I create amazing web experiences with modern technologies.
+            Specializing in Both front-end  and backend development, I build Apis and interfaces that are
+            both beautiful and functional. I&apos;m Serious, curious, agile, patient and passionate about information
+             technolog.  I&apos;m able to work
+               independently and as part of a team.
+          </p>
+
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View my work
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
